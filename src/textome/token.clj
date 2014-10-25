86f8daaabfb5ca42e50fcf34c91e1cc124f8df54
bitfl1pper <bitfl1pper@users.noreply.github.com>
2014-10-24 20:05:31 -0400

    rename fn to align with similar f.utility / ngram fns, clean up ns, considered merging this software with ngram/f.utility

--- src/textome/token.clj
+++ src/textome/token.clj
@@ -1,14 +1,14 @@
 (ns textome.token
   (:require [clojure.string :as st]))
 
-(defn tokens-space
+(defn tokenize-space
   "Simple tokenizer, splits strings on whitespace.
    Accepts a string, e.g. \"This is test.\"
    Returns a vector of tokens, e.g. [\"This\" \"is\" \"test.\"]"
   [s]
   (st/split s #"\s"))
 
-(defn tokens-word
+(defn tokenize-wordchar
   "Simple tokenizer, splits strings on not words.
    Accepts a string, e.g. \"This is test.\"
    Returns a vector of tokens, e.g. [\"This\" \"is\" \"test\"]"
@@ -20,7 +20,7 @@
 ;;;; for repl play
 ;;;; ALL BELOW IS TEMPORARY, TODELETE
 
-(defn loadfile
+(defn read-file-by-line
   "Load in file for parsing.
    This is from the f.utility lib, change ns.
    fix later"
@@ -36,23 +36,3 @@
   "Counts how many distinct items in col."
   [col]
   (count (distinct col)))
-
-(comment
-  "copied from scratch buffer #1"
-  "Repl play"
-  (def commandtxt "home/command.txt")
-  (def command-tokens (flatten (map (partial tokens-space) (loadfile command))))
-  textome.token> (count (flatten (map (partial tokens-space) (loadfile commandtxt))))
-  36848
-  (drop-if "" (flatten (map (partial tokens-space) (loadfile commandtxt))))
-  textome.token> (count (drop-if "" (flatten (map (partial tokens-space) (loadfile commandtxt)))))
-  36329
-  textome.token> (- 36848 36329)
-  519
-  (unique (drop-if "" (flatten (map (partial tokens-space) (loadfile commandtxt)))))
-  textome.token> (unique (drop-if "" (flatten (map (partial tokens-space) (loadfile commandtxt)))))
-  8229
-  "There are 8229 'unique tokens' in this essay. N. Stephenson conveyed his ideas with a token alphabet of size 8229."
-  "of course, this includes tokens liek \word\ and \word.\ "
-
-  )
